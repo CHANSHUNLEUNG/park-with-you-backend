bfb089ea4e007ef0a3341e13535fe6d588574787
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-24 15:56:30 +0800

    [OrderRequest] add order request model

---
+++ src/main/java/com/intelligentcat/parkwithyoubackend/model/OrderRequest.java
@@ -0,0 +1,12 @@
+package com.intelligentcat.parkwithyoubackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private String customerId;
+}
