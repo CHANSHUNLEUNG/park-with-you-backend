96433e09f6ab9b6ed5caca6ac63ac74a9487b9a5
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-24 20:12:01 +0800

    [Remove Unused files]

--- src/test/java/com/intelligentcat/parkwithyoubackend/BookingControllerTests.java
+++ src/test/java/com/intelligentcat/parkwithyoubackend/BookingControllerTests.java
@@ -1,28 +1,33 @@
 package com.intelligentcat.parkwithyoubackend;
 
-import com.intelligentcat.parkwithyoubackend.controller.ParkingLotBookingAPI;
+import com.intelligentcat.parkwithyoubackend.controller.BookingController;
 import com.intelligentcat.parkwithyoubackend.model.OrderRequest;
+import com.intelligentcat.parkwithyoubackend.service.BookingService;
 import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import io.restassured.module.mockmvc.response.MockMvcResponse;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 
 @SpringBootTest
-public class ParkingLotBookingAPITests {
+public class BookingControllerTests {
+    @Autowired
+    BookingService bookingService;
+
     @Before
     public void setUp(){
-        RestAssuredMockMvc.standaloneSetup(new ParkingLotBookingAPI());
+        RestAssuredMockMvc.standaloneSetup(new BookingController(bookingService));
     }
 
     @Test
     public void should_get_receipt_when_sent_booking_request(){
-        OrderRequest orderRequest = new OrderRequest(12312);
+        OrderRequest orderRequest = new OrderRequest(12312, new Long(12312), 3600);
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .body(orderRequest)
                 .when()
