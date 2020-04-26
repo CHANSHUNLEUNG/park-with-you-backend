021ab2424092dd5c7b768f5b4a3cbc61fccafa64
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-26 17:27:38 +0800

    [CustomerRepository] +getCustomerByName() for login handling

--- src/main/java/com/intelligentcat/parkwithyoubackend/repository/CustomerRepository.java
+++ src/main/java/com/intelligentcat/parkwithyoubackend/repository/CustomerRepository.java
@@ -25,12 +25,17 @@ public class CustomerRepository {
         }
     }
 
-    public CustomerRepository(JdbcTemplate jdbcTemplate){
+    public CustomerRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Customer> getAllCustomer(){
+    public List<Customer> getAllCustomer() {
         final String sql = "select * from customer";
         return jdbcTemplate.query(sql, new CustomerRowMapper());
+    }
+
+    public List<Customer> getCustomerByName(String customerName) {
+        final String sql = "select * from customer where name = ?;";
+        return jdbcTemplate.query(sql, new Object[]{customerName}, new CustomerRowMapper());
     }
 }
