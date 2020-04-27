d6b4aa9333d0e4c4cd2b651e646e4050264c716c
Matt Cheung <matt.cheung@oocl.com>
2020-04-27 15:15:57 +0800

    [Matt] change ordercontroller route of booking, add function getorderlist by customer

--- src/main/java/com/intelligentcat/parkwithyoubackend/controller/OrderController.java
+++ src/main/java/com/intelligentcat/parkwithyoubackend/controller/OrderController.java
@@ -1,13 +1,16 @@
 package com.intelligentcat.parkwithyoubackend.controller;
 
+import com.intelligentcat.parkwithyoubackend.model.OrderDetail;
 import com.intelligentcat.parkwithyoubackend.model.OrderRequest;
 import com.intelligentcat.parkwithyoubackend.model.OrderResponse;
 import com.intelligentcat.parkwithyoubackend.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/parking-lots/{parkingLotId}/booking")
+@RequestMapping
 public class OrderController {
     private OrderService orderService;
 
@@ -16,9 +19,14 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @PostMapping
+    @PostMapping("/parking-lots/{parkingLotId}/booking")
     public OrderResponse addNewBooking(@PathVariable("parkingLotId") Integer parkingLotId,
                                        @RequestBody OrderRequest orderRequest) {
         return orderService.addNewBooking(parkingLotId, orderRequest);
+    }
+
+    @GetMapping("/parking-orders/customers/{customerId}")
+    public List<OrderDetail> getOrderListByCustomer(@PathVariable Integer customerId) {
+        return orderService.getOrderListByCustomer(customerId);
     }
 }
