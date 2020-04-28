d4169a4daf181c3a4d0d31b2fa092c2b78c96972
Calvin Chow <calvin.chow@oocl.com>
2020-04-28 16:23:55 +0800

    [Calvin] display order detail with coupon id

--- src/main/java/com/intelligentcat/parkwithyoubackend/model/OrderDetail.java
+++ src/main/java/com/intelligentcat/parkwithyoubackend/model/OrderDetail.java
@@ -1,4 +1,5 @@
 package com.intelligentcat.parkwithyoubackend.model;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,15 +8,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDetail {
-	String orderTime;
-	Integer orderId;
-	Integer customerId;
-	Integer parkingLotId;
-	String parkingLotName;
-	String address;
-	Double unitPrice;
-	Integer parkingPlaceId;
-	String parkingPlaceName;
-	String startParkingTime;
-	Integer duration;
+    String orderTime;
+    Integer orderId;
+    Integer customerId;
+    Integer parkingLotId;
+    String parkingLotName;
+    String address;
+    Double unitPrice;
+    Integer parkingPlaceId;
+    String parkingPlaceName;
+    String startParkingTime;
+    Integer duration;
+    Integer couponId;
 }
--- src/main/java/com/intelligentcat/parkwithyoubackend/repository/OrderRepository.java
+++ src/main/java/com/intelligentcat/parkwithyoubackend/repository/OrderRepository.java
@@ -14,106 +14,99 @@ import java.util.List;
 
 @Repository
 public class OrderRepository {
-	private JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
 
-	public OrderRepository(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+    public OrderRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-	public OrderResponse createNewOrder(String now, ParkingPlace availablePlace, OrderRequest orderRequest) {
-		Integer customerId = orderRequest.getCustomerId();
-		Integer parkingLotId = availablePlace.getParkingLotId();
-		Integer parkingPlaceId = availablePlace.getId();
-		String parkingPlaceName = availablePlace.getName();
-		String orderTime = now;
-		String startParkingTime = orderRequest.getStartTime();
-		Integer parkingDuration = orderRequest.getDuration();
+    public OrderResponse createNewOrder(String now, ParkingPlace availablePlace, OrderRequest orderRequest) {
+        Integer customerId = orderRequest.getCustomerId();
+        Integer parkingLotId = availablePlace.getParkingLotId();
+        Integer parkingPlaceId = availablePlace.getId();
+        String parkingPlaceName = availablePlace.getName();
+        String orderTime = now;
+        String startParkingTime = orderRequest.getStartTime();
+        Integer parkingDuration = orderRequest.getDuration();
 
-		final String sql = "insert into `order` (customer_id, parking_place_id, order_time, start_parking_time, parking_duration) values (?, ?, ?, ?, ?)";
-		KeyHolder keyHolder = new GeneratedKeyHolder();
-		jdbcTemplate.update(
-						new PreparedStatementCreator() {
-							@Override
-							public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-								PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-								ps.setInt(1, customerId);
-								ps.setInt(2, parkingPlaceId);
-								ps.setString(3, orderTime);
-								ps.setString(4, startParkingTime);
-								ps.setInt(5, parkingDuration);
-								return ps;
-							}
-						},
-						keyHolder
-		);
-		Integer orderId = keyHolder.getKey().intValue();
-		return new OrderResponse(orderTime,
-						orderId,
-						customerId,
-						parkingLotId,
-						parkingPlaceId,
-						parkingPlaceName,
-						startParkingTime,
-						parkingDuration);
-	}
+        final String sql = "insert into `order` (customer_id, parking_place_id, order_time, start_parking_time, parking_duration) values (?, ?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(
+                new PreparedStatementCreator() {
+                    @Override
+                    public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                        PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                        ps.setInt(1, customerId);
+                        ps.setInt(2, parkingPlaceId);
+                        ps.setString(3, orderTime);
+                        ps.setString(4, startParkingTime);
+                        ps.setInt(5, parkingDuration);
+                        return ps;
+                    }
+                },
+                keyHolder
+        );
+        Integer orderId = keyHolder.getKey().intValue();
+        return new OrderResponse(orderTime,
+                orderId,
+                customerId,
+                parkingLotId,
+                parkingPlaceId,
+                parkingPlaceName,
+                startParkingTime,
+                parkingDuration);
+    }
 
-	public Order getOrderById(Integer parkingPlaceId) {
-		final String sql = "select * from `order` where id=?;";
-		List<Order> orders = jdbcTemplate.query(
-				sql,
-				new Object[]{parkingPlaceId},
-				(response, rowNumber) ->{
-					return new Order(
-							response.getString("order_time"),
-							response.getInt("id"),
-							response.getInt("customer_id"),
-							response.getInt("parking_place_id"),
-							response.getString("start_parking_time"),
-							response.getInt("parking_duration")
-					);
-				});
-		if(orders.size()==0){
-			throw new NoAvailablePlaceException();
-		}
-		return orders.get(0);
-	}
+    public Order getOrderById(Integer parkingPlaceId) {
+        final String sql = "select * from `order` where id=?;";
+        List<Order> orders = jdbcTemplate.query(
+                sql,
+                new Object[]{parkingPlaceId},
+                (response, rowNumber) -> {
+                    return new Order(
+                            response.getString("order_time"),
+                            response.getInt("id"),
+                            response.getInt("customer_id"),
+                            response.getInt("parking_place_id"),
+                            response.getString("start_parking_time"),
+                            response.getInt("parking_duration")
+                    );
+                });
+        if (orders.size() == 0) {
+            throw new NoAvailablePlaceException();
+        }
+        return orders.get(0);
+    }
 
-	public Order extendParkingBookingTime(Order order, Integer duration) {
-		final String sql = "update `order` set parking_duration = parking_duration + ? where id= ?;";
-		jdbcTemplate.update(sql, new Object[]{duration, order.getOrderId()});
-		order.setDuration(order.getDuration() + duration);
-		return order;
-	}
+    public Order extendParkingBookingTime(Order order, Integer duration) {
+        final String sql = "update `order` set parking_duration = parking_duration + ? where id= ?;";
+        jdbcTemplate.update(sql, new Object[]{duration, order.getOrderId()});
+        order.setDuration(order.getDuration() + duration);
+        return order;
+    }
 
-	private class OrderDetailRowMapper implements RowMapper<OrderDetail> {
-		@Override
-		public OrderDetail mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
-			OrderDetail orderDetail = new OrderDetail();
-        orderDetail.setOrderTime(resultSet.getString("o.order_time"));
-        orderDetail.setOrderId(resultSet.getInt("o.id"));
-        orderDetail.setCustomerId(resultSet.getInt("o.customer_id"));
-        orderDetail.setParkingLotId(resultSet.getInt("pl.id"));
-        orderDetail.setParkingLotName(resultSet.getString("pl.name"));
-        orderDetail.setAddress(resultSet.getString("pl.address"));
-        orderDetail.setUnitPrice(resultSet.getDouble("pl.unit_price"));
-        orderDetail.setParkingPlaceId(resultSet.getInt("pp.id"));
-        orderDetail.setParkingPlaceName(resultSet.getString("pp.name"));
-        orderDetail.setStartParkingTime(resultSet.getString("o.start_parking_time"));
-        orderDetail.setDuration(resultSet.getInt("o.parking_duration"));
-			return orderDetail;
-		}
-	}
+    private class OrderDetailRowMapper implements RowMapper<OrderDetail> {
+        @Override
+        public OrderDetail mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrderTime(resultSet.getString("order_time"));
+            orderDetail.setOrderId(resultSet.getInt("order_id"));
+            orderDetail.setCustomerId(resultSet.getInt("customer_id"));
+            orderDetail.setParkingLotId(resultSet.getInt("parking_lot_id"));
+            orderDetail.setParkingLotName(resultSet.getString("parking_lot_name"));
+            orderDetail.setAddress(resultSet.getString("address"));
+            orderDetail.setUnitPrice(resultSet.getDouble("unit_price"));
+            orderDetail.setParkingPlaceId(resultSet.getInt("parking_place_id"));
+            orderDetail.setParkingPlaceName(resultSet.getString("parking_place_name"));
+            orderDetail.setStartParkingTime(resultSet.getString("start_parking_time"));
+            orderDetail.setDuration(resultSet.getInt("parking_duration"));
+            orderDetail.setCouponId(resultSet.getInt("coupon_id"));
+            return orderDetail;
+        }
+    }
 
-	public List<OrderDetail> findJointDetailByCustomerId(Integer customerId) {
-      final String sql = "select \n" +
-              "o.order_time, o.id, o.customer_id, pl.id, pl.name, pl.address, pl.unit_price, pp.id, pp.name, o.start_parking_time, o.parking_duration\n" +
-              "from `order` o\n" +
-              "inner join `parking_place` pp\n" +
-              "on o.parking_place_id = pp.id\n" +
-              "inner join `parking_lot` pl\n" +
-              "on pl.id = pp.parking_lot_id\n" +
-              "where o.customer_id = ?\n" +
-              "order by o.start_parking_time desc, o.order_time desc;";
-      return jdbcTemplate.query(sql, new Object[]{customerId}, new OrderDetailRowMapper());
-	}
+    public List<OrderDetail> findJointDetailByCustomerId(Integer customerId) {
+        final String sql = "SELECT o.order_time `order_time` ,o.id `order_id` ,o.customer_id `customer_id` ,pl.id `parking_lot_id` ,pl.name `parking_lot_name` ,pl.address `address` ,pl.unit_price `unit_price` ,pp.id `parking_place_id` ,pp.name `parking_place_name` ,o.start_parking_time `start_parking_time` ,o.parking_duration `parking_duration` ,c.id `coupon_id` FROM `order` o INNER JOIN `parking_place` pp ON pp.id = o.parking_place_id INNER JOIN `parking_lot` pl ON pl.id = pp.parking_lot_id LEFT JOIN ( SELECT * FROM `coupon` WHERE customer_id = ? AND STATUS = 'used' ) c ON c.used_order_id = o.id WHERE o.customer_id = ? ORDER BY o.start_parking_time DESC ,o.order_time DESC;";
+        return jdbcTemplate.query(sql, new Object[]{customerId, customerId}, new OrderDetailRowMapper());
+    }
 }
