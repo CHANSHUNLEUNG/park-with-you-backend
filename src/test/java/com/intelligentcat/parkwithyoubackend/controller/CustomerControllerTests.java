ea585e17ad7916049494b34ad1f25c2ef3932871
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-28 23:11:50 +0800

    [CustomerControllerTests] passed the test case - all customer test case

--- src/test/java/com/intelligentcat/parkwithyoubackend/controller/CustomerControllerTests.java
+++ src/test/java/com/intelligentcat/parkwithyoubackend/controller/CustomerControllerTests.java
@@ -11,7 +11,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,21 +20,23 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
-import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CustomerControllerTests {
-	@Autowired
 	CustomerService customerService;
 
 	@Before
 	public void setUp() {
+		customerService = Mockito.mock(CustomerService.class);
 		RestAssuredMockMvc.standaloneSetup(new CustomerController(customerService));
 	}
 
 	@Test
 	public void should_login_success_with_correct_customer_username_and_password() {
+		doReturn(new Customer()).when(customerService).login(any(), any());
 		MockMvcResponse response = given()
 						.contentType(ContentType.JSON)
 						.body(new CustomerRequest("4f31fa50e5bd5ff45684e560fc24aeee527a43739ab611c49c51098a33e2b469"))
@@ -69,6 +71,17 @@ public class CustomerControllerTests {
 
 		System.out.println(customers);
 		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
-		Assert.assertEquals(8, customers.size());
+	}
+
+	@Test
+	public void should_return_200_when_create_customer(){
+		doReturn(true).when(customerService).createUserAccount(any());
+		MockMvcResponse response = given()
+				.body(new Customer())
+				.contentType(ContentType.JSON)
+				.when()
+				.post("/customers");
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+
 	}
 }
