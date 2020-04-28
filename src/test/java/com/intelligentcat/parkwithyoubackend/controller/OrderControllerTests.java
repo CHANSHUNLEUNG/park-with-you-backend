6096c5ea5290681c525b0e0671c7e85a4389fdf1
Matt Cheung <matt.cheung@oocl.com>
2020-04-28 21:31:02 +0800

    [Matt] pass test: should_get_order_detail_lot_list_by_customer

--- src/test/java/com/intelligentcat/parkwithyoubackend/controller/OrderControllerTests.java
+++ src/test/java/com/intelligentcat/parkwithyoubackend/controller/OrderControllerTests.java
@@ -1,9 +1,11 @@
 package com.intelligentcat.parkwithyoubackend.controller;
 
 import com.intelligentcat.parkwithyoubackend.controller.OrderController;
+import com.intelligentcat.parkwithyoubackend.model.OrderDetail;
 import com.intelligentcat.parkwithyoubackend.model.OrderRequest;
 import com.intelligentcat.parkwithyoubackend.service.OrderService;
 import io.restassured.http.ContentType;
+import io.restassured.mapper.TypeRef;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import io.restassured.module.mockmvc.response.MockMvcResponse;
 import org.junit.Assert;
@@ -14,6 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 
@@ -37,5 +42,28 @@ public class OrderControllerTests {
                 .when()
                 .post("/parking-lots/1/booking");
         Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+    }
+
+    @Test
+    public void should_get_order_detail_lot_list_by_customer() {
+        MockMvcResponse response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/parking-orders/customers/1");
+
+        List<OrderDetail> orderDetails = response
+                .getBody()
+                .as(
+                        new TypeRef<List<OrderDetail>>() {
+                            @Override
+                            public Type getType() {
+                                return super.getType();
+                            }
+                        }
+                );
+
+        System.out.println(orderDetails);
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+        Assert.assertEquals(2, orderDetails.size());
     }
 }
