e861bcc4211b0dcc56d1d9705023d6d3eccfcf73
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-28 22:06:06 +0800

    [CouponControllerTests] passed the test case - should_return_shared_link_when_get_by_customer_id_order_id

--- src/test/java/com/intelligentcat/parkwithyoubackend/controller/CouponControllerTests.java
+++ src/test/java/com/intelligentcat/parkwithyoubackend/controller/CouponControllerTests.java
@@ -1,24 +1,41 @@
 package com.intelligentcat.parkwithyoubackend.controller;
 
+import com.intelligentcat.parkwithyoubackend.model.ShareLink;
 import com.intelligentcat.parkwithyoubackend.service.CouponService;
+import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.response.MockMvcResponse;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CouponControllerTests {
-	@Autowired
 	CouponService couponService;
 
 	@Before
 	public void setUp() {
+		couponService = Mockito.mock(CouponService.class);
 		RestAssuredMockMvc.standaloneSetup(new CouponController(couponService));
 	}
 
+	@Test
+	public void should_return_shared_link_when_get_by_customer_id_order_id(){
+		doReturn(new ShareLink()).when(couponService).getShareLink(any(), any());
+
+		MockMvcResponse response = given().contentType(ContentType.JSON)
+				.when()
+				.get("/coupons/share-link?customerId=1&orderId=1");
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+	}
 }
