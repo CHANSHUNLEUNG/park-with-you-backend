8ced11e88f5a64417972464b2fada56093d9a31c
Matt Cheung <matt.cheung@oocl.com>
2020-04-28 21:13:47 +0800

    [Matt] set up @autowired and @before for all controller tests

--- src/test/java/com/intelligentcat/parkwithyoubackend/controller/CouponControllerTests.java
+++ src/test/java/com/intelligentcat/parkwithyoubackend/controller/CouponControllerTests.java
@@ -1,6 +1,10 @@
 package com.intelligentcat.parkwithyoubackend.controller;
 
+import com.intelligentcat.parkwithyoubackend.service.CouponService;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,5 +13,12 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CouponControllerTests {
+	@Autowired
+	CouponService couponService;
+
+	@Before
+	public void setUp() {
+		RestAssuredMockMvc.standaloneSetup(new CouponController(couponService));
+	}
 
 }
